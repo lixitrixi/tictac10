5cccbd44bc99fe5c0812b748934e56b9d9997b3c
Felix <felix@felixleitner.com>
2021-08-07 19:01:17 -0400

    small start

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
 # Imports
+import sys
+from getopt import getopt
+
+
+# Vars
+width, height = 10, 10
+board = [ [+1 for y in range(height)] for x in range(width) ]
+
+COMP = +1
+HUMAN = -1
+
+# Functions
+def render(state):
+  # displays board to console
+  # state: current state of the board
+
+  chars = {
+    0: " ",
+    +1: "X",
+    -1: "O"
+  }
+
+  for row in state:
+    print(' '.join([chars[cell] for cell in row]))
+
+render(board)
