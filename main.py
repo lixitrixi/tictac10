5bc54d84fdf797e19245d19c53812ab03c280bc2
Felix <felix@felixleitner.com>
2021-08-18 13:10:42 -0400

    main functionality

--- main.py
+++ main.py
@@ -2,18 +2,40 @@
 from board import Board
 import player
 
-board = Board(3, 3, 3)
+def main():
+    board = Board(10, 10, 5)
 
-playerX = player.HumanPlayer()
-playerO = player.BotPlayer('O', 5)
+    playerX = player.BotPlayer('X', 3)
+    playerO = player.BotPlayer('O', 3)
+    
+    while not board.gameover():
+        board.render()
+        moveX = playerX.get_move(board)
+        board.set_move(moveX, 'X')
 
-while not board.gameover():
+        if board.gameover(): break
+
+        board.render()
+        moveO = playerO.get_move(board)
+        board.set_move(moveO, 'O')
+    
     board.render()
-    moveX = playerX.get_move(board)
-    board.set_move(moveX, 'X')
+    if len(board.possible_moves()) == 0:
+        print('Draw!')
+    elif board.iswin('X'):
+        print('Player X has won!')
+    elif board.iswin('O'):
+        print('Player O has won!')
 
-    if board.gameover(): break
+main()
+# board = Board(3, 3, 3)
 
-    board.render()
-    moveO = playerO.get_move(board)
-    board.set_move(moveO, 'O')
+# playerX = player.HumanPlayer()
+# playerO = player.BotPlayer('O', 5)
+
+# board.set_move((2,2), 'X')
+# board.set_move((2,1), 'X')
+
+# board.render()
+
+# print(playerO.get_move(board))
--- player.py
+++ player.py
@@ -1,7 +1,6 @@
 import random
 import string
 import math
-from copy import deepcopy
 
 alphabet = list(string.ascii_uppercase)
 
@@ -41,10 +40,41 @@ class BotPlayer():
         self.search_depth = search_depth
     
     def get_move(self, state):
-        return self.minimax(state, True)['move']
+        return self.minimax(state, 0, -math.inf, math.inf, True)['move']
 
     def minimax(self, state, depth, alpha, beta, maximizing): # returns optimal move
         if state.gameover() or depth == self.search_depth:
             return {'move': None, 'score': state.evaluate(self.player)}
         
-        
+        if maximizing:
+            best = {'move': None, 'score': -math.inf}
+            for move in state.possible_moves():
+                state.set_move(move, self.player)
+                check = self.minimax(state, depth+1, alpha, beta, False)
+
+                state.set_move(move, ' ') # undo move
+                check['move'] = move # attribute move to its resulting board state
+
+                if check['score'] > best['score']:
+                    best = check
+                
+                alpha = max(alpha, best['score'])
+                if best['score'] >= beta:
+                    break
+            return best
+        else:
+            best = {'move': None, 'score': math.inf}
+            for move in state.possible_moves():
+                state.set_move(move, self.other_player)
+                check = self.minimax(state, depth+1, alpha, beta, True)
+
+                state.set_move(move, ' ') # undo move
+                check['move'] = move # attribute move to its resulting board state
+
+                if check['score'] < best['score']:
+                    best = check
+                
+                beta = min(beta, best['score'])
+                if best['score'] <= alpha:
+                    break
+            return best
